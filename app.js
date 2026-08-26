fcd8055daf6429bc49c98ab5e0f3db092fe1145e
MACS-AR <96332901+MACS-AR@users.noreply.github.com>
2026-08-26 23:10:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 // 🔥 تهيئة Firebase
 // =============================================
 const firebaseConfig = {
-  apiKey: "AIzaSyBgRkceRq7FRbhCevLlULYNy-A5Tl_cr0w",
-  authDomain: "sr-test-c9e06.firebaseapp.com",
-  databaseURL: "https://sr-test-c9e06-default-rtdb.firebaseio.com",
-  projectId: "sr-test-c9e06",
-  storageBucket: "sr-test-c9e06.firebasestorage.app",
-  messagingSenderId: "658396508062",
-  appId: "1:658396508062:web:c56cd84f93daa2e176308f",
-  measurementId: "G-3ZZV344NDK"
+    apiKey: "AIzaSyBgRkceRq7FRbhCevLlULYNy-A5Tl_cr0w",
+    authDomain: "sr-test-c9e06.firebaseapp.com",
+    databaseURL: "https://sr-test-c9e06-default-rtdb.firebaseio.com",
+    projectId: "sr-test-c9e06",
+    storageBucket: "sr-test-c9e06.firebasestorage.app",
+    messagingSenderId: "658396508062",
+    appId: "1:658396508062:web:c56cd84f93daa2e176308f"
 };
 
 firebase.initializeApp(firebaseConfig);
